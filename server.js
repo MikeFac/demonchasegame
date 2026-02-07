d66891ff700e7cb16b15116647fe0200a7fe3b64
Michael.Fackerell <michaelfackerell@gmail.com>
2026-02-07 20:52:12 +1000

    Change port to 3500 to avoid conflict

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
