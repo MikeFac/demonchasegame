27eb179a0dbb0907323794f52bc50d76452d8b6c
Michael.Fackerell <michaelfackerell@gmail.com>
2026-04-30 13:22:04 +1000

    fix(print): simplify front QR call to action

--- src/server/print/1page/templates.js
+++ src/server/print/1page/templates.js
@@ -19,7 +19,7 @@ function renderOnePageInner(sheet, page) {
           <div class="onepage-cover-side">
             ${qrMarkup}
             <div class="cover-url">${escapeHtml(sheet.siteUrl.replace(/^https?:\/\//, ''))}</div>
-            <div class="page-footer-cta onepage-front-cta">Scan to memorize and play at<br>VerseBattles.com</div>
+            <div class="page-footer-cta onepage-front-cta">Scan to Play Today</div>
           </div>
         </div>
         <ul class="onepage-front-features">
